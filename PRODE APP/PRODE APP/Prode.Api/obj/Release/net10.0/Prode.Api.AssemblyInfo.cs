80c706bb04d4ea54ba8fad1b7fd4f726aadccf63
Garro, Federico <federico.garro@accenture.com>
2026-05-27 12:56:48 -0300

    Update cache files for Prode.Api with new input hashes and properties

--- PRODE APP/PRODE APP/Prode.Api/obj/Release/net10.0/Prode.Api.AssemblyInfo.cs
+++ PRODE APP/PRODE APP/Prode.Api/obj/Release/net10.0/Prode.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prode.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab21f13ea67c6676f3ef4135e6b8d3fb2b1555d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a226ecf227867bfaab3f7a7573f5599c2ab520")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prode.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prode.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
