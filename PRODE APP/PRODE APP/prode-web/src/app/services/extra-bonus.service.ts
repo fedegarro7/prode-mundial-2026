24b51eebe66cec0d68e2ed024e8fbabda84520fe
Garro, Federico <federico.garro@accenture.com>
2026-07-06 00:29:49 -0300

    fix(extra-bonus): update API URL construction for group extra bonus retrieval

--- PRODE APP/PRODE APP/prode-web/src/app/services/extra-bonus.service.ts
+++ PRODE APP/PRODE APP/prode-web/src/app/services/extra-bonus.service.ts
@@ -2,14 +2,16 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RoundExtraBonuses } from '../models/extra-bonus.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExtraBonusService {
   private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/rankings`;
 
   getGroupExtraBonus(groupId: number, roundKey: string): Observable<RoundExtraBonuses> {
-    return this.http.get<RoundExtraBonuses>(`/api/rankings/group/${groupId}/extra-bonus/${roundKey}`);
+    return this.http.get<RoundExtraBonuses>(`${this.apiUrl}/group/${groupId}/extra-bonus/${roundKey}`);
   }
 }
